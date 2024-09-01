f367d3f79bdc6da526736e3b5bd57a9a2a825aec
Woshika <wkavindi016@gmail.com>
2024-08-31 17:06:48 -1200

    Get Products From Database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); //Connect to MongoDB
@@ -15,14 +16,8 @@ app.get("/", (req, res) => {
   res.send("Hello, World!"); // Send a simple response
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
